24e7afde6cc813cb3c4ac8c7ef0020c6c444d48e
krishpranav <krisna.pranav@gmail.com>
2021-08-08 09:24:29 +0530

    modules: asterisk

--- modules/asterisk.rb
+++ modules/asterisk.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 SIP_BASE_REQUEST = "[METHOD] sip:[DST_URI] SIP/2.0\r\n" \
 "Via: SIP/2.0/[TRANSPORT] [LOCAL_URI]:[LOCAL_PORT];branch=[BRANCH];rport\r\n" \
 "User-Agent: [USERAGENT]\r\n" \
@@ -30,11 +32,9 @@ module AsteriskModule
     ]
 
     @@services_files = [
-
         '/etc/asterisk/sip.conf'            => /^(allow=|allowguest=)/,
         '/etc/asterisk/extensions.conf'     => /^(allow=)/
     ]
-
 
     @@services_tcp_detect_regex = [
         :identifiers    => /(Server:|User-Agent:)/,
@@ -50,8 +50,12 @@ module AsteriskModule
 
     @@extensions = []
 
+    # Check for service informations and vulnerabilities.
+    # This method will be used to manage the alive hosts
+    # found by the tcp port scanner, maybe to identify the
+    # system informations and vulnerabilities.
     def parse_tcp(socket, address, port)
-
+        # Generates payload header.
         header = generate_payload(SIP_BASE_REQUEST.dup, [
             :method     => SIP_METHODS.first[:OPTIONS], 
             :extension  => '7002', 
@@ -79,7 +83,6 @@ module AsteriskModule
         end
     end
 
-
     def parse_udp(socket, address, port, timeout)
 
         header = generate_payload(SIP_BASE_REQUEST.dup, [
@@ -94,7 +97,7 @@ module AsteriskModule
 
         begin
             Timeout.timeout(timeout) {
-                # Validate the possible sip reply.
+
                 buf = read_request(socket, 'udp')
 
                 if buf.to_s.include?('SIP/')
@@ -109,9 +112,6 @@ module AsteriskModule
         end
     end
 
-    # Check for md5 challenge response hash.
-    # Try to grep the handshake before sip authentication
-    # through REGISTER.
     def parse_password(line, packet_time)
         if line =~ /WWW-Authenticate:/
             puts "  %s: #{GB}handshake request data#{RST}\n  -------------------------------------------" % [packet_time]
@@ -126,9 +126,6 @@ module AsteriskModule
         return false
     end
 
-    # Enumeration method.
-    # Perform extensions enumerations with
-    # a custom range.
     def enum(socket, list, address, port, transport, timeout)
         list.each { |e|
             if e.to_s.include?(':~')
@@ -206,9 +203,6 @@ module AsteriskModule
         }
     end
 
-    # Bruteforce method.
-    # Execute extension(s) bruteforce base on 
-    # password file list.
     def bruteforce(socket, extension, address, port, passwords, transport, timeout)
         passwords.each { |p|
             if extension.to_s.include?(':~')
@@ -217,7 +211,6 @@ module AsteriskModule
 
             print "  #{GB}> testing authentication: #{RST}%-16s : %-22s#{RST}\r" % [extension, p]
 
-            # Generate the payload header.
             header = generate_payload(SIP_BASE_REQUEST.dup, [
                 :method     => SIP_METHODS.first[:REGISTER], 
                 :extension  => extension.to_s, 
@@ -336,3 +329,148 @@ module AsteriskModule
 
         return false
     end
+
+    # Send the network request.
+    def send_request(descriptor, transport, header)
+        begin
+            # Send the udp sip request.
+            if transport == 'udp'
+                descriptor.send(header.chomp, 0)
+            else
+                descriptor.puts(header.chomp)
+            end
+
+            return true
+        rescue Exception
+            return false
+        end
+    end
+
+    # Read the network response.
+    def read_request(descriptor, transport)
+        # Validate the possible sip reply.
+        if transport == 'udp'
+            buf, raddr = descriptor.recvfrom(2048)
+        else
+            buf = descriptor.recv(2048)
+        end
+
+        return buf
+    end
+
+    # Parse the tcp/udp network response.
+    def parse_response(address, port, buf, transport)
+        tmp = []
+
+        if transport == 'tcp'
+            regex = @@services_tcp_detect_regex
+        else
+            regex = @@services_udp_detect_regex
+        end
+
+        # Loop through identifiers to match the correct
+        # targeted system informations.
+        buf.to_s.split("\n").each { |line|
+            # Detects service informations.
+            r = regex.first[:identifiers]
+
+            unless r.nil?
+                if line =~ r
+                    name = line.sub(r, '').strip
+                    puts "  + %-16s open port udp/%-15s %s\n" % [address, port, name]
+                    STDOUT
+
+                    tmp = [
+                        :service_address    => address.to_s,
+                        :service_port       => (port.to_s + '/' + transport),
+                        :service_name       => name.to_s,
+                        :service_methods    => ''
+                    ]
+                end
+            end
+
+            # Detect allowed methods from service.
+            r = regex.first[:methods]
+            unless r.nil?
+                if line =~ r
+                    tmp.first[:service_methods] = line.sub(r, '').strip unless tmp.empty?
+                end
+            end
+        }
+
+        unless tmp.empty?
+            @@devices.push(tmp)
+        else
+            puts "  + %-16s open port udp/%-15s Unknown\n" % [address, port]
+            STDOUT
+        end
+    end
+
+    # Generate the payload header.
+    def generate_payload(header, data)
+        data = data.first
+        # Generate the random payload data.
+        est = gen_essentials
+
+        # Replace the occurrencies.
+        header.gsub!('[USERAGENT]',     'vsaudit-ua')
+        header.gsub!('[TRANSPORT]',     data[:transport])
+        header.gsub!('[METHOD]',        data[:method].split(' ').last)
+        header.gsub!('[METHOD_SEQ]',    data[:method])
+        header.gsub!('[DST_ACCOUNT]',   data[:extension])
+        header.gsub!('[DST_URI]',       data[:address])
+        header.gsub!('[LOCAL_ACCOUNT]', data[:extension])
+        header.gsub!('[LOCAL_URI]',     data[:address])
+        header.gsub!('[LOCAL_PORT]',    data[:port])
+        header.gsub!('[BRANCH]',        est[:branch])
+        header.gsub!('[TAG_ID]',        est[:tag_id])
+        header.gsub!('[TAG_ID_CALL]',   est[:tag_id_call])
+
+        return header
+    end
+
+    # Generate the random payload data.
+    def gen_essentials
+        # Basic alphanumeric characters list.
+        list = [('a'..'z'), (0..9)].map { |i| i.to_a }.flatten
+
+        # Generate a branch string.
+        branch_hash = ''
+        branch = (0...15).map { 
+            list[rand(list.length)] 
+        }.join
+
+        branch_append = branch.dup
+        branch_hash.concat('-' + branch_append = (0...4).map { 
+            list[rand(list.length)] 
+        }.join)
+
+        branch_hash.concat('-' + branch_append = (0...4).map { 
+            list[rand(list.length)] 
+        }.join)
+
+        branch_hash.concat('-' + branch_append = (0...4).map { 
+            list[rand(list.length)] 
+        }.join)
+
+        branch_hash.concat('-' + branch_append = (0...12).map { 
+            list[rand(list.length)] 
+        }.join)
+
+        # Generate a tag_id string.
+        tag_id = (0...8).map { 
+            list[rand(list.length)] 
+        }.join
+
+        # Generate a tag_id_call string.
+        tag_id_call = (0...8).map { 
+            list[rand(list.length)] 
+        }.join
+
+        return [
+            :branch         => (branch + branch_hash), 
+            :tag_id         => (tag_id + branch_hash), 
+            :tag_id_call    => (tag_id_call + branch_hash)
+        ].first
+    end
+end
